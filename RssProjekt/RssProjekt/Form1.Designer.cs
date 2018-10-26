60a19cff51f9e270dcbe33083acb724a382c60e5
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2018-10-26 09:16:14 +0200

    .

--- RssProjekt/RssProjekt/Form1.Designer.cs
+++ RssProjekt/RssProjekt/Form1.Designer.cs
@@ -33,6 +33,8 @@
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.URLLable = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listView1
@@ -41,8 +43,8 @@
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3});
-            this.listView1.Location = new System.Drawing.Point(9, 10);
-            this.listView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listView1.Location = new System.Drawing.Point(11, 10);
+            this.listView1.Margin = new System.Windows.Forms.Padding(2);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(250, 190);
             this.listView1.TabIndex = 0;
@@ -63,24 +65,43 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(184, 216);
+            this.button1.Location = new System.Drawing.Point(120, 261);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 220);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // URLLable
+            // 
+            this.URLLable.AutoSize = true;
+            this.URLLable.Location = new System.Drawing.Point(13, 206);
+            this.URLLable.Name = "URLLable";
+            this.URLLable.Size = new System.Drawing.Size(29, 13);
+            this.URLLable.TabIndex = 3;
+            this.URLLable.Text = "URL";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.URLLable);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -91,6 +112,8 @@
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label URLLable;
     }
 }
 
--- RssProjekt/RssProjekt/Form1.cs
+++ RssProjekt/RssProjekt/Form1.cs
@@ -16,5 +16,10 @@ namespace RssProjekt
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
