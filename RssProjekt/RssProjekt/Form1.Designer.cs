b917cae03b0848676fc4fa55ac7fadd14a4d9fff
Amanda <Amanda@>
2018-10-26 13:56:49 +0200

    Lagt till textfält och combobox

--- RssProjekt/RssProjekt/Form1.Designer.cs
+++ RssProjekt/RssProjekt/Form1.Designer.cs
@@ -28,28 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lvPodcast = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbUrl = new System.Windows.Forms.TextBox();
             this.URLLable = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listView1
+            // lvPodcast
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvPodcast.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3});
-            this.listView1.Location = new System.Drawing.Point(11, 10);
-            this.listView1.Margin = new System.Windows.Forms.Padding(2);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(250, 190);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.lvPodcast.Location = new System.Drawing.Point(15, 12);
+            this.lvPodcast.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lvPodcast.Name = "lvPodcast";
+            this.lvPodcast.Size = new System.Drawing.Size(563, 233);
+            this.lvPodcast.TabIndex = 0;
+            this.lvPodcast.UseCompatibleStateImageBehavior = false;
+            this.lvPodcast.View = System.Windows.Forms.View.Details;
             // 
             // columnHeader1
             // 
@@ -65,39 +69,79 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(120, 261);
+            this.button1.Location = new System.Drawing.Point(20, 301);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // tbUrl
             // 
-            this.textBox1.Location = new System.Drawing.Point(11, 220);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(162, 20);
-            this.textBox1.TabIndex = 2;
+            this.tbUrl.Location = new System.Drawing.Point(15, 271);
+            this.tbUrl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(228, 22);
+            this.tbUrl.TabIndex = 2;
             // 
             // URLLable
             // 
             this.URLLable.AutoSize = true;
-            this.URLLable.Location = new System.Drawing.Point(13, 206);
+            this.URLLable.Location = new System.Drawing.Point(17, 254);
+            this.URLLable.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.URLLable.Name = "URLLable";
-            this.URLLable.Size = new System.Drawing.Size(29, 13);
+            this.URLLable.Size = new System.Drawing.Size(36, 17);
             this.URLLable.TabIndex = 3;
             this.URLLable.Text = "URL";
             // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(250, 271);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(201, 22);
+            this.tbName.TabIndex = 4;
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(457, 271);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(121, 24);
+            this.cbCategory.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Namn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(472, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Kategori";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.tbName);
             this.Controls.Add(this.URLLable);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbUrl);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.listView1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.lvPodcast);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -107,13 +151,17 @@
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lvPodcast;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbUrl;
         private System.Windows.Forms.Label URLLable;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.ComboBox cbCategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
