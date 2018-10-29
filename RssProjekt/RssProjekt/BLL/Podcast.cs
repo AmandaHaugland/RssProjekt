d6b933f32978dfb8eab17eabd694b38021063dce
Amanda <Amanda@>
2018-10-29 15:09:02 +0100

    Podcast class

---
+++ RssProjekt/RssProjekt/BLL/Podcast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssProjekt.BLL
+{
+    class Podcast
+    {
+        public string rssUrl { get; set; }
+        public string Namn { get; set; }
+        public string Kategori { get; set; }
+        public string Uppdatering { get; set; }
+    }
+}
