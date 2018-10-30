ce12b55fbf19ebf6bc40d6ea25cce7d50fdd98c6
AmandaH <Amanda@H>
2018-10-30 14:12:32 +0100

    Mindre ändring

--- RssProjekt/RssProjekt/Form1.Designer.cs
+++ RssProjekt/RssProjekt/Form1.Designer.cs
@@ -176,7 +176,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(581, 254);
+            this.label3.Location = new System.Drawing.Point(581, 252);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(142, 17);
             this.label3.TabIndex = 9;
