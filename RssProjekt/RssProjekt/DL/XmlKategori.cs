e927fe1cea65197cbda40c31a90ea0326d6553eb
Amanda <Amanda@>
2018-10-31 12:47:12 +0100

    Nu funkar xml till viss del

--- RssProjekt/RssProjekt/DL/XmlKategori.cs
+++ RssProjekt/RssProjekt/DL/XmlKategori.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.IO;
 
 
 
 namespace RssProjekt.DL
 {
+
     class XmlKategori
     {
-        
+         public void CreateXml()
+        {
+            XmlDocument doc = new XmlDocument();
+            string path = @"XMLKategori.xml";
+            XmlWriter xmlOut = XmlWriter.Create(path);
+            xmlOut.WriteStartDocument();
+            xmlOut.WriteStartElement("Product");
+            xmlOut.WriteElementString("Test", "test");
+            xmlOut.WriteEndElement();
+           
+            xmlOut.Close();
+        }
         public void AddKategoriToXml (string katNamn)
         {
 
