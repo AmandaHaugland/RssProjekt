83f2252aaec8adf57e586eda197cade7ab7bf485
Amanda <Amanda@>
2018-11-01 19:34:00 +0100

    Påbörjat feed

---
+++ RssProjekt/RssProjekt/BLL/Feed.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+ 
+namespace RssProjekt.BLL
+{
+    [Serializable()]
+    class Feed : ISerializable
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PubDate { get; set; }
+
+        public Feed() { }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Title", Title);
+            info.AddValue("Description", Description);
+           // info.AddValue("PubDate", PubDate);
+          //  throw new NotImplementedException();
+        }
+
+        public Feed(SerializationInfo info, StreamingContext context)
+        {
+            Title = (string)info.GetValue("Title", typeof(string));
+            Description = (string)info.GetValue("Description", typeof(string));
+            //PubDate = (string)info.GetValue("PubDate", typeof(string));
+        }
+
+
+    }
+}
---
+++ RssProjekt/RssProjekt/DL/XmlFeed.cs
@@ -0,0 +1,29 @@
+﻿using RssProjekt.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.ServiceModel.Syndication;
+
+namespace RssProjekt.DL
+{
+    class XmlFeed
+    {
+        public List<Feed> makeFeed(string rssUrl)
+        {
+            List<Feed> listOfFeed = new List<Feed>();
+            XmlReader reader = XmlReader.Create(rssUrl);
+            var syndicationFeed = SyndicationFeed.Load(reader);
+            foreach(var item in syndicationFeed.Items)
+            {
+                Feed feed = new Feed();
+                feed.Title = item.Title.Text;
+                feed.Description = item.Summary.Text;
+                listOfFeed.Add(feed);
+            }
+            return listOfFeed;
+        }
+    }
+}
