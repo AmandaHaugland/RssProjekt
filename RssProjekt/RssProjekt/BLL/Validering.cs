8a7269ebedcc8e473311263fa77e6cafe21d9c49
MarinaB2 <44431361+MarinaB2@users.noreply.github.com>
2018-11-02 22:56:27 +0100

    Ett försök

--- RssProjekt/RssProjekt/BLL/Validering.cs
+++ RssProjekt/RssProjekt/BLL/Validering.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,27 +12,31 @@ namespace RssProjekt.BLL
     {
         public bool CheckIf(String url, String namn, int kategori, int uppdatering)
         {
+            Regex rx = new Regex(@"/^(https?:\/\/)[a-zA-Z0-9]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9]+\/[a-zA-Z0-9]+/");
 
             if (string.IsNullOrEmpty(url) ||
                 string.IsNullOrEmpty(namn) ||
                 kategori<= -1 ||
                 uppdatering <= -1
-                )
-            {
+               )
+           {
                 MessageBox.Show("Du måste fylla i fälten!");
                 return false;
                 //Lägg till felmeddelande??
                 
             }
-            else if (!url.Contains(@"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$"))
+
+             else if (url.Equals(rx))
             {
                 MessageBox.Show("Url är fel, försöka igen");
                 return false;
             }
             else
             {
+            
                 return true;
             }
+           
 
 
         }
